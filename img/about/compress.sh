5178fe63c8b35acc4b77d490d13ebecbcbb62c5c
GavFior <gavin@woodsofplenty.com>
2026-05-04 17:35:08 -0600

    island builder
    new song bonus
    quetzal ca mode

--- img/about/compress.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for f in *.png; do
-pngquant --force --ext .png --quality=80-95 "$f"
-echo "Done: $f"
-done
--- img/about/resize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for f in *.png; do
-sips -Z 1920 "$f"
-echo "Done: $f"
-done
